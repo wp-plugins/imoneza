6ef65777e348c6dac4a83a905d5cd5af5889480f
chrisatnws <chrisatnws@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-08 18:46:19 +0000

    added support for target conversion pricing
    git-svn-id: https://plugins.svn.wordpress.org/imoneza/tags/1.2.0@1240904 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class.imoneza-restfulresponse.php
@@ -0,0 +1,15 @@
+<?php
+/*
+class iMoneza_RestfulResponse {
+
+    public $headers;
+    public $body;
+
+    public function __construct($rawResponse)
+    {
+        $this->headers = $rawResponse['headers'];
+        $this->body = $rawResponse['body'];
+    }
+}
+*/
+?>
